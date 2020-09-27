e7f5f697257a7823b5573e20ac5330b5322248ae
OlehStrong <yavtushenkooleh1@gmail.com>
2020-09-27 15:11:39 +0300

    first commit

---
+++ Circle.cpp
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int m;
+	cin >> m;
+	int _count_ = 0;
+
+	for (int i = 0; i < m;) {
+		if (_count_ % 2 == 0) {
+
+			i = i + 10;
+
+		}
+		else {
+
+			i = i + 20;
+		}
+		_count_++;
+		cout << i << endl;
+
+	}
+
+
+	cout << _count_ << endl;
+}
---
+++ randtutorial.cpp
@@ -0,0 +1,31 @@
+﻿
+
+#include<iostream>
+
+#include<ctime>
+using namespace std;
+int main()
+{
+	srand(time(NULL));
+	const int SIZE = 10;
+	int arr[SIZE];
+	int a;
+	
+	for (int i = 0; i < SIZE; i++)
+	{
+		arr[i] = rand() % 10;
+		if (i == 0);
+		else {
+
+			for (int j = i; j >= 0; j--) {
+				if (arr[i] == arr[j]) {
+					j = i;
+					arr[i] = rand() % 10;
+				}
+			}
+		}
+		cout << arr[i] << endl;
+	}
+	
+}
+
