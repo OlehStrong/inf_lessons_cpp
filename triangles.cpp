e8781d6fc22e36a63dc3aee3ca3cb88d6f081b82
OlehStrong <yavtushenkooleh1@gmail.com>
2020-10-04 19:23:12 +0300

    commit

---
+++ triangles.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <fstream>
+using namespace std;
+int main()
+{
+
+	ifstream fileIn("data.in");
+	int s;
+	fileIn >> s;
+	fileIn.close();
+	ofstream fileOut("data.sol");
+	int o = 1;
+
+	for (int i = 0; i < s; i++) 
+	{
+		o = o*2;
+
+	}
+	fileOut << o << endl;
+	fileOut.close();
+
+}
